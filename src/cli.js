992f9a411eae9e4df734f5e1de3b1919b7e0e8c6
SergeySH <sshurdakov@gmail.com>
2020-10-16 16:40:08 +0300

    fix fn greeting

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
-import promptly from "promptly";
+import promptly from 'promptly';
 
 export default async function greeting() {
-  const name = await promptly.prompt("May I have your name? ");
-  console.log(`${"Hello"}, ${name}!`);
+  const name = await promptly.prompt('May I have your name? ');
+  console.log(`${'Hello'}, ${name}!`);
 }
