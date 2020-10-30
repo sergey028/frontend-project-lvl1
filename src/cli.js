57de4f7bb9c35d6152e8723df39fa0c45e605aa0
SergeySH <sshurdakov@gmail.com>
2020-10-30 13:33:45 +0300

    Update and fix fn greeting and brainEven

--- src/cli.js
+++ src/cli.js
@@ -1,52 +1,38 @@
 import promptly from 'promptly';
 
-async const userName = await promptly.prompt('Welcome to the Brain Games! \nMay I have your name? ');
-
 export async function greeting() {
-  const name = userName;
+  const name = await promptly.prompt('May I have your name?');
   console.log(`${'Hello'}, ${name}!`);
-};
+  return name;
+}
 
 function getRandom(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.round(rand);
-};
-
+}
 
 const isEvenNumber = (num) => ((num % 2 === 0));
-  
-  export async function brainEven() {
-    
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
- 
-    
-    for (let i = 0; i < 3; i++) {
-      const randomNumber = getRandom(1, 1000);
-  
-  
-      console.log('Question:', randomNumber);
-  
-  
-      const isEven = isEvenNumber(randomNumber);
-  
-  
-      const correctAnswer = isEven ? 'yes' : 'no';
-  
-      const answer = await promptly.prompt('Your answer:');
-  
-  
-      if (answer !== correctAnswer) {
-        console.log(`${answer} ${'is wrong answer ;(.'} ${'Correct answer was'} ${correctAnswer}`);
-        console.log(`${"Let's try again"}, ${userName}!`);
-        
-        return;
-      } else {
-        console.log('Correct!');
-      }
-      
+export async function brainEven(name) {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandom(1, 1000);
+
+    console.log('Question:', randomNumber);
+
+    const isEven = isEvenNumber(randomNumber);
+
+    const correctAnswer = isEven ? 'yes' : 'no';
+    const answer = await promptly.prompt('Your answer:');
+    if (answer !== correctAnswer) {
+      console.log(`'${answer}' ${'is wrong answer ;(.'} ${'Correct answer was'} '${correctAnswer}'`);
+      console.log(`${"Let's try again"}, ${name}!`);
+
+      return;
     }
-    
-    console.log(`${'Congratulations'}, ${userName}!`);
-   
-  };
+    console.log('Correct!');
+  }
+
+  console.log(`${'Congratulations'}, ${name}!`);
+}
