16a78929f25bc432241326da20e82ad0e3f153a7
SergeySH <sshurdakov@gmail.com>
2020-10-31 19:11:43 +0300

    Update

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import { greeting, brainEven } from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-
-async function triggerGreeting() {
-  const name = await greeting();
-  await brainEven(name);
-}
-
-triggerGreeting();
+(async () => {
+    console.log('Welcome to the Brain Games!');
+  
+    const name = (await greeting());
+  
+    await brainEven(name);
+  })()
