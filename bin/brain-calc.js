2c0e820fd3af960b35f74b0be5ef360182bb669a
SergeySH <sshurdakov@gmail.com>
2020-10-31 19:38:23 +0300

    Add brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { greeting, brainEven } from '../src/cli.js';
+
+(async () => {
+    console.log('Welcome to the Brain Games!');
+  
+    const name = (await greeting());
+  
+    await brainCalc(name);
+  })()
