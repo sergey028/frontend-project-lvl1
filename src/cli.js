80249f17d1dcb9c205cf12af03b6e87c6c7f0e92
SergeySH <sshurdakov@gmail.com>
2020-10-31 19:37:28 +0300

    Add fn brainCalc

--- src/cli.js
+++ src/cli.js
@@ -16,7 +16,7 @@ const isEvenNumber = (num) => ((num % 2 === 0));
 export async function brainEven(name) {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  for (let i = 0; i < 3; i += 1) {
+  for (let round = 0; round < 3; round += 1) {
     const randomNumber = getRandom(1, 1000);
 
     console.log('Question:', randomNumber);
@@ -36,3 +36,30 @@ export async function brainEven(name) {
 
   console.log(`${'Congratulations'}, ${name}!`);
 }
+
+const num1 =  getRandom(1, 1000);
+
+const num2 = getRandom(1, 1000);
+
+сonst operation = ['+', '-', '*'];
+
+async function brainCalc(num1, num2, operation) {
+  console.log('What is the result of the expression?');
+
+switch (operation) {
+case '+' :
+  console.log(`${num1} + ${num2}`);
+  return ('Correct!');
+case '-':
+  console.log(`${num1} - ${num2}`);
+  return ('Correct!');;
+  case '*': 
+  console.log(`${num1} * ${num2}`);
+  return ('Correct!');
+  default: 
+  // в default пока не знаю, что написать
+    return  console.log(`'${answer}' ${'is wrong answer ;(.'} ${'Correct answer was'} '${correctAnswer}'`);
+    console.log(`${"Let's try again"}, ${name}!`);
+} 
+}
+
