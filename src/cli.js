54606072bc96b6138bab0c5cf28e65d9bd91fc59
SergeySH <sshurdakov@gmail.com>
2020-11-01 15:07:40 +0300

    Add update fn brainCalc

--- src/cli.js
+++ src/cli.js
@@ -7,17 +7,18 @@ export async function greeting() {
 }
 
 function getRandom(min, max) {
-  const rand = min + Math.random() * (max + 1 - min);
+  const rand = Math.random() * (max - min) + min;
   return Math.round(rand);
 }
 
 const isEvenNumber = (num) => ((num % 2 === 0));
+const numberOfRounds = 3;
 
 export async function brainEven(name) {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  for (let round = 0; round < 3; round += 1) {
-    const randomNumber = getRandom(1, 1000);
+  for (let round = 0; round < numberOfRounds; round += 1) {
+    const randomNumber = getRandom(1, 100);
 
     console.log('Question:', randomNumber);
 
@@ -37,29 +38,56 @@ export async function brainEven(name) {
   console.log(`${'Congratulations'}, ${name}!`);
 }
 
-const num1 =  getRandom(1, 1000);
+let operations = ['+', '-', '*'];
 
-const num2 = getRandom(1, 1000);
+async function brainCalc(name) {
 
-сonst operation = ['+', '-', '*'];
-
-async function brainCalc(num1, num2, operation) {
   console.log('What is the result of the expression?');
 
-switch (operation) {
-case '+' :
-  console.log(`${num1} + ${num2}`);
-  return ('Correct!');
-case '-':
-  console.log(`${num1} - ${num2}`);
-  return ('Correct!');;
-  case '*': 
-  console.log(`${num1} * ${num2}`);
-  return ('Correct!');
-  default: 
-  // в default пока не знаю, что написать
-    return  console.log(`'${answer}' ${'is wrong answer ;(.'} ${'Correct answer was'} '${correctAnswer}'`);
-    console.log(`${"Let's try again"}, ${name}!`);
-} 
+  for (let round = 0; round < numberOfRounds; round += 1) {
+
+    const num1 = getRandom(1, 100);
+
+    const num2 = getRandom(1, 100);
+
+    const operation = operations[getRandom(0, 2)];
+
+    console.log(`'Question:', ${num1} ${operation} ${num2}`);
+
+     const answer = await promptly.prompt('Your answer:');
+
+    switch (operation) {
+      case '+':
+        console.log(`Question: ${num1} + ${num2}`);
+        const correctAnswer = num1 + num2;
+        break;
+      case '-':
+        console.log(`Question: ${num1} - ${num2}`);
+        const correctAnswer = num1 - num2;
+        break;
+      case '*':
+        console.log(`Question: ${num1} * ${num2}`);
+        const correctAnswer = num1 * num2;
+        break;
+    }
+
+   
+    if (answer !== correctAnswer) {
+      console.log(`'${answer}' ${'is wrong answer ;(.'} ${'Correct aswer was'} '${correctAnswer}'`);
+      console.log(`${"Let's try again"}, ${name}!`);
+      return;
+    }
+    console.log('Correct!')
+  }
+  console.log(`${'Congratulations'}, ${name}!`);
 }
 
+
+// убрать параметры из brainCalc и определить эти переменные внутри - ок
+// operation - случайный элемент из массива operations
+// спрашивать ответ у пользователя после того, как показали вопрос
+
+// посчитать и запомнить правильный ответ
+// сравнить правильный ответ с ответом пользователя
+
+// default нужен только в тех, когда не сработал ни один case
