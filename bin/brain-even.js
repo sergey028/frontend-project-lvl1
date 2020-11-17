a027f70a05f74e9c6ac355ce4f9caaa60f74e4dc
SergeySH <sshurdakov@gmail.com>
2020-11-17 15:00:22 +0300

    Update structure

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,8 +2,6 @@
 import { greeting, brainEven } from '../src/cli.js';
 
 (async () => {
-  console.log('Welcome to the Brain Games!');
-
   const name = (await greeting());
 
   await brainEven(name);
