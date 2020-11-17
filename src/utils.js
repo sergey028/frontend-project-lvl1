7f1c633d907ec1a3f4d3ba744763dd67c637dac2
SergeySH <sshurdakov@gmail.com>
2020-11-17 15:11:46 +0300

    Update fn getRandom

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
-export function getRandom(min, max) {
-    const rand = Math.random() * (max - min) + min;
-    return Math.round(rand);
-  }
-  
+export default function getRandom(min, max) {
+  const rand = Math.random() * (max - min) + min;
+  return Math.round(rand);
+};
