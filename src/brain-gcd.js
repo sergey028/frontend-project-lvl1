ee6fcb106471961a9abbe2fbd8143e9d7a532e7d
SergeySH <sshurdakov@gmail.com>
2020-11-17 15:07:12 +0300

    Update structure

--- src/brain-gcd.js
+++ src/brain-gcd.js
@@ -1,33 +1,24 @@
-import promptly from 'promptly';
+import getRandom  from './utils.js';
 
+const rules = 'Find the greatest common divisor of given numbers.';
 
-export async function brainGcd(name) {
-  console.log('Find the greatest common divisor of given numbers.');
-
-  for (let round = 0; round < numberOfRounds; round += 1) {
-    const num1 = getRandom(1, 100);
-
-    const num2 = getRandom(1, 100);
-
-    const gcd = (number1, number2) => {
-      if (number2 === 0) {
-        return number1;
-      }
-      return gcd(number2, number1 % number2);
-    };
-    
-    console.log(`${num1} ${num2}`);
-
-    const correctAnswer = gcd(num1, num2);
-
-    const userAnswer = await promptly.prompt('Your answer:');
-
-    if (userAnswer !== String(correctAnswer)) {
-      console.log(`'${userAnswer}' ${'is wrong answer ;(.'} ${'Correct aswer was'} '${correctAnswer}'`);
-      console.log(`${"Let's try again"}, ${name}!`);
-      return;
-    }
-    console.log('Correct!');
+const gcd = (number1, number2) => {
+  if (number2 === 0) {
+    return number1;
   }
-  console.log(`${'Congratulations'}, ${name}!`);
+  return gcd(number2, number1 % number2);
+};
+
+export function genereateRound() {
+  const num1 = getRandom(1, 100);
+  const num2 = getRandom(1, 100);
+
+  const question = `${num1} ${num2}`;
+
+  const correctAnswer = gcd(num1, num2);
+
+  return { question, correctAnswer };
+
 }
+
+export const game = { rules, genereateRound };
