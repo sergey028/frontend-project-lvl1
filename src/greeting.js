0de04e50c1a271fdc3e672b1a48d98514552525f
SergeySH <sshurdakov@gmail.com>
2020-11-19 22:51:19 +0300

    Update

--- src/greeting.js
+++ src/greeting.js
@@ -2,7 +2,10 @@ import promptly from 'promptly';
 
 export default async function greeting() {
   console.log('Welcome to the Brain Games!');
+
   const name = await promptly.prompt('May I have your name?');
+
   console.log(`${'Hello'}, ${name}!`);
+
   return name;
-};
+}
