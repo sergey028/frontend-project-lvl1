35f9fd074b8c135004e302d151f18ed110d3cb16
sergey028 <48182143+sergey028@users.noreply.github.com>
2020-11-29 20:39:22 +0300

    Update brain-calc.js

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,18 +2,18 @@ import getRandom from '../utils.js';
 
 const OPERATIONS = ['+', '-', '*'];
 
-function genereateRound() {
-  const num1 = getRandom(1, 100);
-  const num2 = getRandom(1, 100);
-  const operation = OPERATIONS[getRandom(0, OPERATIONS.length - 1)];
-
-  const operationValue = {
+const operationValue = {
     '+': num1 + num2,
 
     '-': num1 - num2,
 
     '*': num1 * num2,
   };
+
+function genereateRound() {
+  const num1 = getRandom(1, 100);
+  const num2 = getRandom(1, 100);
+  const operation = OPERATIONS[getRandom(0, OPERATIONS.length - 1)];
 
   const question = `${num1} ${operation} ${num2}`;
 
